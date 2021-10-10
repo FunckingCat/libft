68e2adf04042412f94ae11824cb3115e56e50a7b
Toad Yamcha <tyamcha@il-h3.msk.21-school.ru>
2021-10-10 15:05:10 +0300

    added memcmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 14:20:53 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/10/10 13:59:15 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/10/10 14:52:22 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_atoi(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
