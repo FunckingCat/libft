bf1c3142051f17bb568e448e42e99100317567ac
David Zh <david99111@mail.com>
2021-10-11 09:56:33 +0300

    Memchr_fix

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,16 +14,19 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	size_t	i;
+
+	i = 0;
 	if (!(char *)s)
 		return (NULL);
 	if (n == 0)
 		return (NULL);
-	while (n-- > 0)
+	while (i < n)
 	{
 		if (*(char *)s == (char)c)
 			return ((void *)s);
 		s++;
-		n--;
+		i++;
 	}
 	return (NULL);
 }
