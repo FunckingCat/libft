da3868c263944c580b37310d33d47cc5e6ceac82
Toad Yamcha <tyamcha@su-d1.msk.21-school.ru>
2021-10-13 13:58:05 +0300

    Fix_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,66 +6,66 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 14:34:30 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/10/10 11:22:06 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/10/13 13:57:36 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t size)
-{
-	register char *d = dst;
-	register const char *s = src;
-	register size_t n = size;
-	size_t dlen;
-
-	while (n-- != 0 && *d != '\0')
-		d++;
-	dlen = d - dst;
-	n = size - dlen;
-
-	if (n == 0)
-		return(dlen + ft_strlen(s));
-	while (*s != '\0') {
-		if (n != 1) {
-			*d++ = *s;
-			n--;
-		}
-		s++;
-	}
-	*d = '\0';
-
-	return(dlen + (s - src));
-}
-
-// size_t	ft_strlcat(char *dst, const char *src, size_t size)
+// size_t ft_strlcat(char *dst, const char *src, size_t size)
 // {
-// 	char		*d;
-// 	const char	*s;
-// 	size_t		n;
-// 	int			buff_size;
+// 	char		*d = dst;
+// 	const char	*s = src;
+// 	size_t		n = size;
+// 	size_t		dlen;
 
-// 	d = dst;
-// 	s = src;
-// 	n = 0;
-// 	buff_size = ft_strlen(src) + ft_strlen(dst);
-// 	if (size < ft_strlen(dst))
-// 		buff_size = ft_strlen(src) + size;
-// 	while (*d)
-// 	{
-// 		n++;
+// 	while (n-- != 0 && *d != '\0')
 // 		d++;
-// 	}
-// 	while (*s)
-// 	{
-// 		if (n + 1 >= size)
-// 			break ;
-// 		*d++ = *s++;
-// 		n++;
+// 	dlen = d - dst;
+// 	n = size - dlen;
+// 	if (n == 0)
+// 		return (dlen + ft_strlen(s));
+// 	while (*s != '\0') {
+// 		if (n != 1) {
+// 			*d++ = *s;
+// 			n--;
+// 		}
+// 		s++;
 // 	}
 // 	*d = '\0';
-// 	return (buff_size);
+// 	return(dlen + (s - src));
 // }
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	char		*d;
+	const char	*s;
+	size_t		n;
+	int			buff_size;
+
+	d = dst;
+	s = src;
+	n = 0;
+	buff_size = ft_strlen(src) + ft_strlen(dst);
+	if (size < ft_strlen(dst))
+		buff_size = ft_strlen(src) + size;
+	while (*d && n < size)
+	{
+		n++;
+		d++;
+	}
+	if (n == size)
+		return (buff_size);
+	while (*s)
+	{
+		if (n + 1 >= size)
+			break ;
+		*d++ = *s++;
+		n++;
+	}
+	*d = '\0';
+	return (buff_size);
+}
 
 // int main ()
 // {
