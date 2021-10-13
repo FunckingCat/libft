62cca3c9042a1c967f1b46e3767f0a155862a917
Toad Yamcha <tyamcha@su-d1.msk.21-school.ru>
2021-10-13 15:52:35 +0300

    added_lstiter

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 14:20:53 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/10/13 15:29:42 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/10/13 15:41:32 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,5 +70,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
