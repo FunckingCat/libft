d944bb2fa7af27b98ef7c7092069e8c3fc01588e
Toad Yamcha <tyamcha@su-d1.msk.21-school.ru>
2021-10-13 15:03:00 +0300

    added_lst_size

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 14:20:53 by tyamcha           #+#    #+#             */
-/*   Updated: 2021/10/13 14:52:53 by tyamcha          ###   ########.fr       */
+/*   Updated: 2021/10/13 14:58:35 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,6 @@ typedef struct s_list
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 #endif
