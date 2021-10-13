2d93b1021bfe02ada33c9f9b0321069db60a2e09
David Zh <david99111@mail.com>
2021-10-13 11:20:42 +0300

    strlcat_in_process

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,31 +12,68 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
-	char		*d;
-	const char	*s;
-	size_t		n;
-	int			buff_size;
+	register char *d = dst;
+	register const char *s = src;
+	register size_t n = size;
+	size_t dlen;
 
-	d = dst;
-	s = src;
-	n = 0;
-	buff_size = ft_strlen(src) + ft_strlen(dst);
-	if (size < ft_strlen(dst))
-		buff_size = ft_strlen(src) + size;
-	while (*d)
-	{
-		n++;
+	while (n-- != 0 && *d != '\0')
 		d++;
-	}
-	while (*s)
-	{
-		if (n + 1 >= size)
-			break ;
-		*d++ = *s++;
-		n++;
+	dlen = d - dst;
+	n = size - dlen;
+
+	if (n == 0)
+		return(dlen + ft_strlen(s));
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
+		}
+		s++;
 	}
 	*d = '\0';
-	return (buff_size);
+
+	return(dlen + (s - src));
 }
+
+// size_t	ft_strlcat(char *dst, const char *src, size_t size)
+// {
+// 	char		*d;
+// 	const char	*s;
+// 	size_t		n;
+// 	int			buff_size;
+
+// 	d = dst;
+// 	s = src;
+// 	n = 0;
+// 	buff_size = ft_strlen(src) + ft_strlen(dst);
+// 	if (size < ft_strlen(dst))
+// 		buff_size = ft_strlen(src) + size;
+// 	while (*d)
+// 	{
+// 		n++;
+// 		d++;
+// 	}
+// 	while (*s)
+// 	{
+// 		if (n + 1 >= size)
+// 			break ;
+// 		*d++ = *s++;
+// 		n++;
+// 	}
+// 	*d = '\0';
+// 	return (buff_size);
+// }
+
+// int main ()
+// {
+// 	char *dest;
+
+// 	dest = malloc(15);
+// 	ft_memset(dest, 'r', 15);
+// 	printf("%s\n", dest);
+// 	printf("%ld    %s\n", ft_strlcat(dest, "lorem ipsum dolor sit amet", 5), dest);
+// 	return (0);
+// }
